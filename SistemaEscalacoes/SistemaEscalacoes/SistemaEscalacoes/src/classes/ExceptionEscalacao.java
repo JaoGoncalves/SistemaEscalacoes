17c01c14fa09f9e19d5411ad0cd5b12ea392a16f
João Vitor Sutil Gonçalves <152215143+JaoGoncalves@users.noreply.github.com>
2025-06-24 21:21:12 -0300

    Delete SistemaEscalacoes/SistemaEscalacoes directory

--- SistemaEscalacoes/SistemaEscalacoes/SistemaEscalacoes/src/classes/ExceptionEscalacao.java
+++
@@ -1,9 +0,0 @@
-package classes;
-
-public class ExceptionEscalacao extends Exception { // criação da exceção personalizada
-
-    // criação do construtor sobrecarregado enviando um paramêtro do tipo String
-    public ExceptionEscalacao(String enviarMensagem) {
-        super(enviarMensagem); // quando chamado, irá enviar uma mensagem, explicando o erro,etc.
-    }
-}
